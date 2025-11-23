3e21518b2c31982aadf3af59897a696fef472c83
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2025-11-22 21:48:33 -0300

    Receita 14

---
+++ app/novarota/componentes.js
@@ -0,0 +1,5 @@
+export function MariaPrea({texto}){
+  return (
+     <h3>{texto}</h3>
+  )
+}
---
+++ app/novarota/subrota/page.js
@@ -0,0 +1,11 @@
+import NovaRotaHome from "../page";
+import { MariaPrea } from "../componentes";
+
+export default function Subpage() {
+  return (
+    <>
+      <NovaRotaHome/>
+      <MariaPrea texto="Morreu Maria Prea"/>
+    </>
+  )
+}
